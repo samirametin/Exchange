77e77c52a309cd79962bcc05180d2a25c8ee5954
samirametin <samire.mahmudova1996@gmail.com>
2022-12-10 02:13:56 +0400

    js beginnings

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+let basebtns=document.querySelector(".btns_1")
+basebtns.addEventListener("click",function(e){
+    var base=e.target.innerText;
+    let symbolbtns=document.querySelector(".btns_2")
+symbolbtns.addEventListener("click",function(e){
+    var symbol=e.target.innerText;
+})
+
+fetch(`https://api.exchangerate.host/latest?base=${base}&symbol=${symbol}`)
+.then(res=> res.json())
+.then(res=> alert(res))
+
+})
 
